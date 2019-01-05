35c8e08fb543cb7761ecfc0117235505527f348f
babq <1153791778@qq.com>
2019-01-05 16:09:31 +0800

    fix patch apply error

--- patches/install_new.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ../../../..
-cd frameworks/av
-git apply -v ../../device/vivo/y67/patches/0007-Disable-usage-of-get_capture_position.patch
-git apply -v ../../device/vivo/y67/patches/0008-Partial-Revert-Camera1-API-Support-SW-encoders-for-n.patch
-git apply -v ../../device/vivo/y67/patches/0009-add-mtk-color-format-support.patch
-cd ../..
