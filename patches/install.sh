7e05491a98576933f3ca2190a0641b4ae0ba4f06
babq <1153791778@qq.com>
2019-01-06 15:17:58 +0800

    fix build error for rros

--- patches/install.sh
+++ patches/install.sh
@@ -60,3 +60,9 @@ git apply -v ../../device/vivo/y67/patches/0007-Disable-usage-of-get_capture_pos
 git apply -v ../../device/vivo/y67/patches/0008-Partial-Revert-Camera1-API-Support-SW-encoders-for-n.patch
 git apply -v ../../device/vivo/y67/patches/0009-add-mtk-color-format-support.patch
 cd ../..
+# for rros
+# ----------------------------------
+cd packages/providers/DownloadProvider
+#git apply -v ../../device/vivo/y67/patches/0006-unbuild-downloadprovider-for-rros.patch
+cd ../../..
+# ----------------------------------
