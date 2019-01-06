cb9c5abbcca8cdb54a2abe79a9f8786cf5b69eb9
babq <1153791778@qq.com>
2019-01-06 08:58:26 +0800

    make patch for sudamod

--- patches/install.sh
+++ patches/install.sh
@@ -60,3 +60,6 @@ git apply -v ../../device/vivo/y67/patches/0007-Disable-usage-of-get_capture_pos
 git apply -v ../../device/vivo/y67/patches/0008-Partial-Revert-Camera1-API-Support-SW-encoders-for-n.patch
 git apply -v ../../device/vivo/y67/patches/0009-add-mtk-color-format-support.patch
 cd ../..
+cd vendor/sm
+git apply -v ../../device/vivo/y67/patches/sudamod-fix-build-bootanimation-error.patch
+cd ../..
